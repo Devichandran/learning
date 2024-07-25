050aac03d745b312c5c47600ea9c8ef3d4651f61
ADMIN <ADMIN@ADMIN>
2024-07-25 19:58:09 +0530

    learning program

---
+++ mavenproject/src/main/java/aggregaton/Aggregationpro.java
@@ -0,0 +1,25 @@
+package aggregaton;
+
+public class Aggregationpro{
+	String home,lane,dist;
+	int houno;
+	int pincode;
+	
+	
+
+	
+
+
+
+
+	public Aggregationpro(String home,String lane ,int dis,int pincode) {
+		
+		this.home=home;
+		this.lane=lane;
+		this.houno=houno;
+		this.pincode=pincode;
+		
+
+	}
+
+}
